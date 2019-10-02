ff7b2f6a1676e79052416ef46c3bf2bff5e3c38f
dipakparmar <hi@dipak.tech>
2019-10-02 13:02:21 -0700

    Added log in MainActivity

--- app/src/main/java/com/dipak/tourguide/MainActivity.java
+++ app/src/main/java/com/dipak/tourguide/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.telephony.mbms.StreamingServiceInfo;
+import android.util.Log;
 
 import com.dipak.tourguide.dummy.DummyContent;
 
@@ -17,11 +19,13 @@ public class MainActivity extends AppCompatActivity implements PlacesFragment.On
 
     @Override
     public void onListFragmentInteraction(DummyContent.DummyItem item) {
+        Log.i("Item", String.valueOf(item));
 
     }
 
     @Override
     public void onFragmentInteraction(Uri uri) {
+        Log.i("Item", String.valueOf(uri));
 
     }
 }
