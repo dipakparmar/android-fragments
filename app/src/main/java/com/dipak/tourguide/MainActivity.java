291d5cc6dc9323232f6ab4219bd9740d385ce663
Dipak Parmar <dipakparmar@Dipaks-MacBook-Pro.local>
2019-10-02 12:47:19 -0700

    Initial commit

---
+++ app/src/main/java/com/dipak/tourguide/MainActivity.java
@@ -0,0 +1,27 @@
+package com.dipak.tourguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.dipak.tourguide.dummy.DummyContent;
+
+public class MainActivity extends AppCompatActivity implements PlacesFragment.OnListFragmentInteractionListener,DetailsFragment.OnFragmentInteractionListener {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
